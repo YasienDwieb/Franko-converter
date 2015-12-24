bb04f69a04e473e87c13cf0a3a98755947ea2444
Yasien.Dwieb <Yasien.Dwieb@gmail.com>
2015-12-24 16:01:16 +0200

    First commit

---
+++ TryaAgain/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TryaAgain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(44, 54);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(214, 40);
+            this.Input.TabIndex = 0;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(44, 181);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(214, 40);
+            this.Output.TabIndex = 1;
+            // 
+            // Run
+            // 
+            this.Run.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Run.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.Run.Location = new System.Drawing.Point(72, 99);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(131, 34);
+            this.Run.TabIndex = 2;
+            this.Run.Text = "Convert";
+            this.Run.UseVisualStyleBackColor = false;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type your text here :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Your converted Text is :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Al-Mo7awel Conan ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TryaAgain/Form1.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TryaAgain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            string readyText ="";
+            string result = "";
+            string temp = "";
+            temp =Input.Text.ToLower();
+            string[] EnglishWords;
+            EnglishWords = temp.Split(' ');
+            for (int i = 0; i < EnglishWords.Length; i++)
+            {
+                readyText += EnglishWords[i] + ',';
+            }
+            readyText = readyText.Substring(0,readyText.Length-1);
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create("http://www.google.com/transliterate/indic?tlqt=1&langpair=en|ar&text=" + readyText + "&&tl_app=1");
+            Request.Method = "GET";
+            Request.ContentType = "application/x-www-form-urlencoded";
+            var Response = (HttpWebResponse)Request.GetResponse();
+            var Reader = new StreamReader(Response.GetResponseStream());
+            var JsonText = Reader.ReadToEnd();
+            var Objects = JArray.Parse(JsonText); // parse as array  
+
+            foreach (JObject jObject in Objects)
+            {
+                List<string> lstArabicWords;
+                foreach (KeyValuePair<String, JToken> pair in jObject)
+                {
+                    lstArabicWords = new List<string>();
+                    if (pair.Key.ToString() == "hws")
+                    {
+                        var ArabicWords = pair.Value;
+                        foreach (var word in ArabicWords)
+                        {
+                            lstArabicWords.Add(word.ToString());
+                            result+= lstArabicWords.ElementAt(0)+" ";
+                            break;
+                        }
+                    }
+                    Output.Text ="";
+                    Output.Text = result;
+                }
+            }
+        }
+    }
+}
